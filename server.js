37b3f357ebcf89b6de68face13dcf0910f2bac08
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-11-10 16:22:59 -0500

    Switch port default to 5001

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express(); 
-const port = process.env.PORT || 5000; 
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5001;
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
 // create a GET route
-app.get('/express_backend', (req, res) => { //Line 9
+app.get('/express_backend', (req, res) => {
+  //Line 9
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
 });
