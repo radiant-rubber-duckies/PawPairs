5b3bacae4276becc30933968bcc045eca1f88333
Caitlin Pauze Sherman <caitlinpSherman@gmail.com>
2022-11-10 20:10:14 -0500

    basic profile

--- front-end/src/Components/Profile.js
+++ front-end/src/Components/Profile.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Main } from './style';
 
+const user = {username: 'sallyskellington',
+location: 11103,
+careStatus: 'OFFERING',
+bio: 'Loves animals and cooking new recipes'
+}
+
 const Profile = () => {
   return (
     <Main className="profile">
-      <h4>
-        Profile
-      </h4>
+      <h3>User Name: {user.username}</h3>
+      <h3>Location: {user.location}</h3>
+      <h3>About: {user.bio}</h3>
+      <h3>I am currently {user.careStatus} care</h3>
     </Main>
   );
 };
